22b2a77fbf49ef03f83395197b4449cc132aaf4d
Lzq550891679 <550891679@qq.com>
2020-02-27 21:47:23 +0800

    Update request.js

--- src/network/request.js
+++ src/network/request.js
@@ -3,7 +3,7 @@ import axios from 'axios';
 export function request(config) {
   // 1.创建axios实例
   const instance = axios.create({
-    baseURL: 'http://123.207.32.32:8000',
+    baseURL: 'http://123.207.xxx.xxx:8000',
     timeout: 5000
   })
   // 2.拦截器
